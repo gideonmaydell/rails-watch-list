9dd11d95e915a18e4298399dcb2c92de4a9b9323
Gideon Maydell <gideon@maydell.at>
2022-11-17 17:10:08 +0100

    movie list app with controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists do
+    resources :bookmarks, only: %i[new index create]
+  end
+  resources :bookmarks, only: :destroy
 end
